212b3ac5e98e9ecd580d81e6c032c0751436fcf3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-21 00:58:41 -0800

    Update to work with new build system.

--- teapot.rb
+++ teapot.rb
@@ -7,10 +7,12 @@ required_version "0.5"
 
 define_target "opengl3-headers" do |target|
 	target.install do |environment|
+		config = environment.flatten
+		
 		include_path = File.expand_path("../include", __FILE__)
 		
-		$stderr.puts "Copying #{include_path} to #{config.install_prefix}"
-		FileUtils.cp_r include_path, config.install_prefix.to_s
+		$stderr.puts "Copying #{include_path} to #{config[:install_prefix]}"
+		FileUtils.cp_r include_path, config[:install_prefix]
 	end
 	
 	target.provides "Aggregate/OpenGL/Headers"

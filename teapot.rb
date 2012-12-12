607e54bcd1df6b401d9003088ec97ac07baaaffd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-12 21:03:23 +1300

    Ensure that headers are copied into correct platform path

--- teapot.rb
+++ teapot.rb
@@ -14,6 +14,9 @@ define_target "opengl3-headers" do |target|
 		$stderr.puts "Copying #{include_path} to #{config[:install_prefix]}"
 		FileUtils.cp_r include_path, config[:install_prefix]
 	end
-	
+
+	target.depends :platform
+		
 	target.provides "Aggregate/OpenGL/Headers"
 end
+

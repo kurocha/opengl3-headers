de68c258eca6fb48e5a6eca53f0a14b68f01652e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-18 00:08:11 +1200

    Preparing for latest release of teapot

--- teapot.rb
+++ teapot.rb
@@ -3,20 +3,18 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.8.0"
+teapot_version "1.0.0"
 
 define_target "opengl3-headers" do |target|
-	target.build do |environment|
-		config = environment.flatten
+	target.build do
+		source_root = target.package.path + 'source'
 		
-		include_path = File.expand_path("../include", __FILE__)
-		
-		$stderr.puts "Copying #{include_path} to #{config[:install_prefix]}"
-		FileUtils.cp_r include_path, config[:install_prefix]
+		copy headers: Files::Glob.new(source_root, 'include/**/*.{h,hpp}'), prefix: environment[:install_prefix]
 	end
-
+	
+	target.depends "Build/Files"
 	target.depends :platform
-		
+	
 	target.provides "Aggregate/OpenGL/Headers"
 end
 
